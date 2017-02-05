af5339f11efcbeb31f9e3a6b4024a5e349eee4e2
Claire <Claire@pc60.home>
2017-02-05 15:42:00 +0100

    add index of rooms and create booking for a room

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,57 @@
+class BookingsController < ApplicationController
+  def index
+    # Get current_user room => to do add devise
+    @rooms = Room.all
+    @selected_date = params[:selected_date]||Date.today
+    @booking = Booking.new
+  end
+
+  def create
+    booking = Booking.new()
+    booking.client = Client.find(booking_params[:client_id])
+    booking.room = Room.find(booking_params[:room_id])
+    booking.start_time = build_start_time
+    booking.end_time = build_end_time
+    booking.save
+    redirect_to bookings_path, selected_date: build_start_time
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit("start_time(1i)",
+                                    "start_time(2i)",
+                                    "start_time(3i)",
+                                    "start_time(4i)",
+                                    "start_time(5i)",
+                                    "end_time(1i)",
+                                    "end_time(2i)",
+                                    "end_time(3i)",
+                                    "end_time(4i)",
+                                    "end_time(5i)",
+                                    "client_id",
+                                    "room_id"
+                                    )
+
+  end
+
+  # à refactoriser et déplacer
+  def build_start_time
+    DateTime.new(booking_params["start_time(1i)"].to_i,
+                booking_params["start_time(2i)"].to_i,
+                booking_params["start_time(3i)"].to_i,
+                booking_params["start_time(4i)"].to_i,
+                booking_params["start_time(5i)"].to_i
+    )
+  end
+
+  # A refactoriser et déplacer
+  def build_end_time
+    DateTime.new(booking_params["end_time(1i)"].to_i,
+                booking_params["end_time(2i)"].to_i,
+                booking_params["end_time(3i)"].to_i,
+                booking_params["end_time(4i)"].to_i,
+                booking_params["end_time(5i)"].to_i
+    )
+  end
+end
